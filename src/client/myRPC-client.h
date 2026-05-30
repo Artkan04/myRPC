70898236b023acc0dbc73b17d9736fe01e364fa5
Artem <artem_kan_2017@mail.ru>
2026-05-30 10:34:02 +0300

    Исправлены ошибки в клиенте

--- src/client/myRPC-client.h
+++ src/client/myRPC-client.h
@@ -2,11 +2,11 @@
 #define MYRPC_CLIENT_H
 
 typedef struct {
-char host[256];
-int port;
-int socket_type;
-char command[4096];
-int show_help;
+    char host[256];
+    int port;
+    int socket_type;
+    char command[4096];
+    int show_help;
 } ClientConfig;
 
 void print_help(const char *prog_name);
@@ -14,3 +14,5 @@ int parse_args(int argc, char *argv[], ClientConfig *config);
 int connect_to_server(const ClientConfig *config);
 int send_request(int socket, const char *username, const char *command);
 int receive_response(int socket);
+
+#endif
